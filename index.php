fb868fe10ef7f88ab1ea46cec78754d83b6ce7eb
João Vitor Landsmann <jvvlandsmann@gmail.com>
2024-07-30 23:02:52 -0300

    testando

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+    
+    include_once("templates1/header.php");
+
+    include_once("templates2/landing.php");
+    include_once("templates2/sobre.php");
+    include_once("templates2/profs.php");
+    include_once("templates2/agenda.php");
+    include_once("templates2/precos.php");
+    include_once("templates2/endereço.php");    
+
+    include_once("templates1/footer.php");
+
+?>
